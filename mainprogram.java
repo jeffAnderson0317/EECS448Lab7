2574b54f06d0199d6424841a488c91a2aea00781
Jeffery Anderson <JefferyMorganAnderson@gmail.com>
2017-03-31 01:33:00 -0500

    Small changes to matrix class and completed main file

--- mainprogram.java
+++ mainprogram.java
@@ -19,15 +19,19 @@ public class mainprogram {
 		{
 			try {
 				BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-			    String line = "";
-			    while (line != null) {
-			        line = br.readLine();
-			        int n = Integer.parseInt(line);
-				  
-				    matrix m = new matrix(n);
-					
+			    String line = br.readLine();
+			    while (line != null && !line.contains("0") && line != "") {
+			    	int n = Integer.parseInt(line);
+			    	matrix m = new matrix(n);
+			        for (int i =0; i < n; i++){
+			        	line = br.readLine();
+			        	String arr[] = line.split(" ");
+			        	for (int j = 0; j<arr.length; j++){
+			        		m.mdata[i][j] = Double.parseDouble(arr[j]);
+			        	}
+			        }
 					out.println("M = ");
-					out.println(m);
+					out.println(m.toString());
 					
 					double det = m.determinant();
 					out.println("det(M) = " + det + "\n");
@@ -36,8 +40,9 @@ public class mainprogram {
 					{
 						matrix inv = m.inverse();
 
-						out.println("Minv= \n " + inv);
+						out.println("Minv= \n " + inv.toString());
 					}
+					line = br.readLine();
 			    }
 			    br.close();
 			}
@@ -47,9 +52,10 @@ public class mainprogram {
 			finally{
 				
 			}
-			
+			out.println("Done!");
 		} catch (IOException e) {
 		    //exception handling left as an exercise for the reader
+			System.out.println(e.getMessage());
 		}
 
 	}
--- matrix.java
+++ matrix.java
@@ -1,9 +1,9 @@
 package example;
 
 public class matrix {
-	private static int n = 10;
+	private int n = 10;
 	private final static int MaxSize = 10;
-	private double mdata[][] = new double[MaxSize][MaxSize];
+	public double mdata[][] = new double[MaxSize][MaxSize];
 	
 	public matrix(int m){
 		n = m;
@@ -22,7 +22,7 @@ public class matrix {
 		}
 		else
 		{
-			for (int i = 0; i < n; ++i)
+			for (int i = 0; i < n; i++)
 			{
 				det += Math.pow(-1.0, (double)i) * mdata[0][i] * subMatrix(0, i).determinant();
 			}
@@ -35,9 +35,9 @@ public class matrix {
 		matrix inv = new matrix(n);
 		double det = determinant();
 		
-		for (int i = 0; i < n; ++i)
+		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < n; ++j)
+			for (int j = 0; j < n; j++)
 			{
 				inv.mdata[i][j] = Math.pow(-1.0, (double)i + j) * subMatrix(j, i).determinant() / det;
 			}
@@ -47,24 +47,33 @@ public class matrix {
 	}
 	public matrix subMatrix (int r, int c){
 		matrix sub = new matrix(n - 1);
-		
 		int row = 0; 
-		for (int i = 0; i < n; ++i)
+		for (int i = 0; i < n; i++)
 		{
 			if (i == r) continue;
 			
 			int col = 0;
-			for (int j = 0; j < n; ++j)
+			for (int j = 0; j < n; j++)
 			{
 				if (j == c) continue;
 				
 				sub.mdata[row][col] = mdata[i][j];
-				++col;
+				col++;
 			}
 			
-			++row;
+			row++;
 		}
 		
 		return sub;
+	}
+	public String toString(){
+		String sm = "";
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < n; j++){
+				sm += mdata[i][j] + " ";
+			}
+			sm += "\n";
+		}
+		return sm;
 	}
 }
